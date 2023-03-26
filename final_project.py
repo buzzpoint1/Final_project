ba1709af51391cd9bed8509cc8e3ee7462b52e77
buzzpoint <sajid_waris@yahoo.com>
2023-03-26 18:33:59 -0400

    finalProject

---
+++ final_project.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# In[5]:
+
+
+df = pd.read_csv("~/Desktop/Final_Projectt/monthly_SingleHome_family.csv")
+
+
+# In[6]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+plt.figure(figsize=(12,4))
+
