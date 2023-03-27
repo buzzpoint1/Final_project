353bffdafb66754118b0bba3fa985948ad3a266a
buzzpoint <sajid_waris@yahoo.com>
2023-03-26 22:17:07 -0400

    update

--- final_project.py
+++ final_project.py
@@ -1,32 +1,63 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[137]:
 
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+import csv
 
 
-# In[5]:
+# In[147]:
 
 
-df = pd.read_csv("~/Desktop/Final_Projectt/monthly_SingleHome_family.csv")
+df = pd.read_csv('monthly_SingleHome_family.csv')
 
 
-# In[6]:
+# In[148]:
 
 
-df.head()
+df.head(10)
 
 
-# In[10]:
+# In[141]:
 
 
 plt.figure(figsize=(12,4),dpi=200)
-sns.scatterplot(x=df['Median Sale Price'],
-                y=df['Average Sale Price'])
+sns.scatterplot(x=df['Median_Sale_Price'],
+                y=df['Average_Sale_Price'])
+
+
+# In[143]:
+
+
+#missing values
+
+missingValues = df[(df['Median_Sale_Price']=='0.00')]
+print(missingValues.head(10))
+
+
+# In[136]:
+
+
+#colums heading into list
+list(df.columns)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
