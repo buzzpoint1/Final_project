2fcda1bc4fe0dae053de7c3d4b7241601fa10754
buzzpoint <sajid_waris@yahoo.com>
2023-03-28 21:23:33 -0400

    Further Analyze

--- final_project.py
+++ final_project.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[208]:
+# In[341]:
 
 
 import pandas as pd
@@ -10,15 +10,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
 import csv
+import datetime
 
 
-# In[209]:
+# In[342]:
 
 
 df = pd.read_csv('monthly_SingleHome_family.csv')
 
 
-# In[210]:
+# In[343]:
 
 
 #missing value testing
@@ -26,14 +27,14 @@ df = pd.read_csv('monthly_SingleHome_family.csv')
 df.isnull()
 
 
-# In[211]:
+# In[344]:
 
 
 #count total missing volues
 pd.isnull(df).sum().sum()
 
 
-# In[212]:
+# In[345]:
 
 
 #detect a list of missing values
@@ -43,13 +44,13 @@ missing=df.isin(missing_Valus)
 missing.head
 
 
-# In[219]:
+# In[346]:
 
 
 df.head(15)
 
 
-# In[214]:
+# In[347]:
 
 
 plt.figure(figsize=(12,4),dpi=200)
@@ -57,7 +58,7 @@ sns.scatterplot(x=df['Median_Sale_Price'],
                 y=df['Average_Sale_Price'])
 
 
-# In[215]:
+# In[348]:
 
 
 #missing values
@@ -66,33 +67,33 @@ missingValues = df[(df['Median_Sale_Price']=='')]
 print(missingValues.head(10))
 
 
-# In[216]:
+# In[349]:
 
 
 #colums heading into list
 list(df.columns)
 
 
-# In[223]:
+# In[350]:
 
 
 dfaapl = pd.read_csv('AAPL.csv')
 
 
-# In[228]:
+# In[351]:
 
 
 #checking to see values working
 dfaapl.head()
 
 
-# In[226]:
+# In[352]:
 
 
 dfgoog = pd.read_csv('GOOG.csv')
 
 
-# In[230]:
+# In[353]:
 
 
 
@@ -100,7 +101,7 @@ dfgoog = pd.read_csv('GOOG.csv')
 dfgoog.head()
 
 
-# In[233]:
+# In[354]:
 
 
 #reading Data from Files using Import CSV loop and list
@@ -118,15 +119,68 @@ print(count)
 myAapl.close()
 
 
-# In[234]:
+# In[355]:
 
 
 dfaapl['Close'].plot()
 plt.show()
 
 
+# In[361]:
+
+
+#Stock graph 
+dfaapl['Close'].plot(label='Stocks At Closing', figsize=(16,5))
+dfaapl['Open'].plot(label='Stock Prices at Open')
+dfaapl['High'].plot(label='Stock Prices at High')
+dfaapl['Low'].plot(label='Stock Prices at Low')
+plt.legend()
+plt.title('Apple Stock Prices')
+plt.ylabel('Stock Prices')
+plt.show()
+
+
+# In[363]:
+
+
+#Comparing Apple and Google at Close to see how we can pridict one stock by another stock
+dfaapl['Close'].plot(label='Apple at Close', figsize=(16,5))
+dfgoog['Close'].plot(label='Google at Close')
+
+plt.legend()
+plt.title(' Stocks Close Prices')
+plt.ylabel('Stock Prices')
+plt.show()
+
+
+# In[364]:
+
+
+#Comparing Apple and Google at Open to see how we can pridict one stock by another stock
+dfaapl['Open'].plot(label='Apple at Open', figsize=(16,5))
+dfgoog['Open'].plot(label='Google at Open')
+
+plt.legend()
+plt.title(' Stocks Open Prices')
+plt.ylabel('Stock Prices')
+plt.show()
+
+
+# In[366]:
+
+
+#Comparing Apple and Google Volume to see how we can pridict one stock by another stock
+dfaapl['Volume'].plot(label='Apple Volume', figsize=(16,5))
+dfgoog['Volume'].plot(label='Google Volume')
+
+plt.legend()
+plt.title(' Stocks Volume')
+plt.ylabel('Stock Prices')
+plt.show()
+
+
 # In[ ]:
 
 
-dfaapl['Close'].plot(Label='Stocks At Closing', figsize=)
+
 
