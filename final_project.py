6bbcd580e09fb3027bf0bd07bc88bbecae74d297
buzzpoint <sajid_waris@yahoo.com>
2023-03-28 22:42:06 -0400

    sums and total traded

--- final_project.py
+++ final_project.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[341]:
+# In[405]:
 
 
 import pandas as pd
@@ -10,16 +10,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
 import csv
-import datetime
+import datetime as dt
+import matplotlib.dates as dates
 
 
-# In[342]:
+# In[406]:
 
 
 df = pd.read_csv('monthly_SingleHome_family.csv')
 
 
-# In[343]:
+# In[407]:
 
 
 #missing value testing
@@ -27,14 +28,14 @@ df = pd.read_csv('monthly_SingleHome_family.csv')
 df.isnull()
 
 
-# In[344]:
+# In[408]:
 
 
 #count total missing volues
 pd.isnull(df).sum().sum()
 
 
-# In[345]:
+# In[409]:
 
 
 #detect a list of missing values
@@ -44,13 +45,30 @@ missing=df.isin(missing_Valus)
 missing.head
 
 
-# In[346]:
+# In[410]:
 
 
 df.head(15)
 
 
-# In[347]:
+# In[443]:
+
+
+#Get the mean for Average Sale Price column
+
+df['Average_Sale_Price'].mean()
+
+
+
+# In[444]:
+
+
+#Sum for Average Sale Price Column
+
+df['Average_Sale_Price'].sum()
+
+
+# In[445]:
 
 
 plt.figure(figsize=(12,4),dpi=200)
@@ -58,7 +76,7 @@ sns.scatterplot(x=df['Median_Sale_Price'],
                 y=df['Average_Sale_Price'])
 
 
-# In[348]:
+# In[446]:
 
 
 #missing values
@@ -67,33 +85,34 @@ missingValues = df[(df['Median_Sale_Price']=='')]
 print(missingValues.head(10))
 
 
-# In[349]:
+# In[447]:
 
 
 #colums heading into list
 list(df.columns)
 
 
-# In[350]:
+# In[448]:
 
 
-dfaapl = pd.read_csv('AAPL.csv')
+# Aapple with dates
+dfaapl = pd.read_csv('AAPL.csv',index_col='Date',parse_dates= True)
 
 
-# In[351]:
+# In[449]:
 
 
 #checking to see values working
 dfaapl.head()
 
 
-# In[352]:
+# In[450]:
 
 
-dfgoog = pd.read_csv('GOOG.csv')
+dfgoog = pd.read_csv('GOOG.csv',index_col='Date',parse_dates= True)
 
 
-# In[353]:
+# In[451]:
 
 
 
@@ -101,7 +120,7 @@ dfgoog = pd.read_csv('GOOG.csv')
 dfgoog.head()
 
 
-# In[354]:
+# In[452]:
 
 
 #reading Data from Files using Import CSV loop and list
@@ -119,14 +138,14 @@ print(count)
 myAapl.close()
 
 
-# In[355]:
+# In[453]:
 
 
 dfaapl['Close'].plot()
 plt.show()
 
 
-# In[361]:
+# In[454]:
 
 
 #Stock graph 
@@ -140,20 +159,19 @@ plt.ylabel('Stock Prices')
 plt.show()
 
 
-# In[363]:
+# In[455]:
 
 
 #Comparing Apple and Google at Close to see how we can pridict one stock by another stock
 dfaapl['Close'].plot(label='Apple at Close', figsize=(16,5))
 dfgoog['Close'].plot(label='Google at Close')
-
 plt.legend()
 plt.title(' Stocks Close Prices')
 plt.ylabel('Stock Prices')
 plt.show()
 
 
-# In[364]:
+# In[456]:
 
 
 #Comparing Apple and Google at Open to see how we can pridict one stock by another stock
@@ -166,7 +184,7 @@ plt.ylabel('Stock Prices')
 plt.show()
 
 
-# In[366]:
+# In[457]:
 
 
 #Comparing Apple and Google Volume to see how we can pridict one stock by another stock
@@ -177,6 +195,45 @@ plt.legend()
 plt.title(' Stocks Volume')
 plt.ylabel('Stock Prices')
 plt.show()
+
+
+# In[460]:
+
+
+#Analyzing the Market Gap. First add Column 'Total Traded Aapleinto CSV'
+dfaapl['Total Traded']= dfaapl['Open']*dfaapl['Volume']
+
+# Check to see if Column was added
+dfaapl
+
+
+# In[462]:
+
+
+#Now add Total Traded Column into Google Stock
+dfgoog['Total Traded']= dfgoog['Open']*dfgoog['Volume']
+
+# Check to see if Column was added
+dfgoog
+
+
+# In[463]:
+
+
+#plotting Total traded for my stocks 
+dfaapl['Total Traded'].plot(label='Apple Total Price Traded', figsize=(16,5))
+dfgoog['Total Traded'].plot(label='Google Total Price Traded')
+
+plt.legend()
+plt.title(' Total Stokcs')
+plt.ylabel('Total Stock Traded')
+plt.show()
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
